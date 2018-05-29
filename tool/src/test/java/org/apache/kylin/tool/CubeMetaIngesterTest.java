9931bf89dadc1fa407f63e6da0ab7059b0e3f852
feng.zhu <fishcus@outlook.com>
2018-05-29 17:09:40 +0800

    minor, fix CubeMetaIngester UT

--- tool/src/test/java/org/apache/kylin/tool/CubeMetaIngesterTest.java
+++ tool/src/test/java/org/apache/kylin/tool/CubeMetaIngesterTest.java
@@ -18,9 +18,15 @@
 
 package org.apache.kylin.tool;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
+import com.google.common.base.Preconditions;
 import org.apache.kylin.common.KylinConfig;
+import org.apache.kylin.common.util.CliCommandExecutor;
 import org.apache.kylin.common.util.LocalFileMetadataTestCase;
 import org.apache.kylin.cube.CubeInstance;
 import org.apache.kylin.cube.CubeManager;
@@ -34,15 +40,17 @@ import org.hamcrest.Description;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
 
-@Ignore
 public class CubeMetaIngesterTest extends LocalFileMetadataTestCase {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Before
     public void setUp() throws Exception {
@@ -57,8 +65,8 @@ public class CubeMetaIngesterTest extends LocalFileMetadataTestCase {
     @Test
     public void testHappyIngest() {
         String srcPath = Thread.currentThread().getContextClassLoader().getResource("cloned_cube_and_model.zip").getPath();
-        CubeMetaIngester.main(new String[] { "-project", "default", "-srcPath", srcPath });
-
+        CubeMetaIngester.main(new String[] { "-project", "default", "-srcPath", srcPath, "-overwriteTables", "true",
+                "-forceIngest", "true", "-restoreType", "cube" });
         ProjectInstance project = ProjectManager.getInstance(KylinConfig.getInstanceFromEnv()).getProject("default");
         Assert.assertEquals(1, Collections.frequency(project.getTables(), "DEFAULT.TEST_KYLIN_FACT"));
         Assert.assertTrue(project.getModels().contains("cloned_model"));
@@ -72,8 +80,8 @@ public class CubeMetaIngesterTest extends LocalFileMetadataTestCase {
     @Test
     public void testHappyIngest2() {
         String srcPath = Thread.currentThread().getContextClassLoader().getResource("benchmark_meta.zip").getPath();
-        CubeMetaIngester.main(new String[] { "-project", "default", "-srcPath", srcPath, "-overwriteTables", "true" });
-
+        CubeMetaIngester.main(new String[] { "-project", "default", "-srcPath", srcPath, "-overwriteTables", "true",
+                "-forceIngest", "true", "-restoreType", "cube"  });
         ProjectInstance project = ProjectManager.getInstance(KylinConfig.getInstanceFromEnv()).getProject("default");
         Assert.assertEquals(1, Collections.frequency(project.getTables(), "SSB.CUSTOMER"));
         Assert.assertTrue(project.getModels().contains("benchmark_model"));
@@ -85,7 +93,7 @@ public class CubeMetaIngesterTest extends LocalFileMetadataTestCase {
     }
 
     @Test
-    public void testBadIngest() {
+    public void testBadIngest() throws IOException {
         thrown.expect(RuntimeException.class);
 
         //should not break at table duplicate check, should fail at model duplicate check
@@ -93,7 +101,7 @@ public class CubeMetaIngesterTest extends LocalFileMetadataTestCase {
             @Override
             public boolean matches(Object item) {
                 if (item instanceof IllegalStateException) {
-                    if (((IllegalStateException) item).getMessage().equals("Already exist a model called test_kylin_inner_join_model_desc")) {
+                    if (((IllegalStateException) item).getMessage().equals("Already exist a model called ci_inner_join_model")) {
                         return true;
                     }
                 }
@@ -105,8 +113,10 @@ public class CubeMetaIngesterTest extends LocalFileMetadataTestCase {
             }
         });
 
-        String srcPath = this.getClass().getResource("/cloned_cube_meta.zip").getPath();
-        CubeMetaIngester.main(new String[] { "-project", "default", "-srcPath", srcPath });
+        String srcPath = doExtractorProject();
+        CubeMetaIngester.main(new String[] { "-project", "default", "-srcPath", srcPath, "-overwriteTables", "true",
+                "-forceIngest", "false", "-restoreType", "project"  });
+
     }
 
     @Test
@@ -116,7 +126,64 @@ public class CubeMetaIngesterTest extends LocalFileMetadataTestCase {
         thrown.expectCause(CoreMatchers.<IllegalStateException> instanceOf(IllegalStateException.class));
 
         String srcPath = this.getClass().getResource("/cloned_cube_meta.zip").getPath();
-        CubeMetaIngester.main(new String[] { "-project", "Xdefault", "-srcPath", srcPath });
+        CubeMetaIngester.main(new String[] { "-project", "Xdefault", "-srcPath", srcPath, "-overwriteTables", "true",
+                "-forceIngest", "true", "-restoreType", "cube" });
+    }
+
+    @Test
+    public void testBackupProjectNotExist() throws IOException {
+        String srcPath = doExtractorProject();
+        // delete the project
+        ProjectManager projectManager = ProjectManager.getInstance(KylinConfig.getInstanceFromEnv());
+        ProjectInstance project = projectManager.getProject("default");
+        project.getRealizationEntries().clear();
+
+        projectManager.dropProject("default");
+
+        project = projectManager.getProject("default");
+        Assert.assertNull(project);
+        // backup project
+        CubeMetaIngester.main(new String[] { "-project", "default", "-srcPath", srcPath, "-overwriteTables", "true",
+                "-forceIngest", "true", "-restoreType", "project" });
+
+        projectManager.reloadProjectQuietly("default");
+        project = projectManager.getProject("default");
+
+        Assert.assertNotNull(project);
+
+    }
+
+    private String doExtractorProject() throws IOException {
+        // Does not support ingest hybrid yet so removeProject before extractorProject
+        ProjectManager projectManager = ProjectManager.getInstance(KylinConfig.getInstanceFromEnv());
+        projectManager.removeRealizationsFromProjects(RealizationType.HYBRID, "ci_inner_join_hybrid");
+
+        folder.create();
+        File tempDir = folder.getRoot();
+        String tempDirAbsPath = tempDir.getAbsolutePath();
+        List<String> args = new ArrayList<>();
+        args.add("-destDir");
+        args.add(tempDirAbsPath);
+        args.add("-project");
+        args.add("default");
+        args.add("-compress");
+        args.add("false");
+        args.add("-includeSegments");
+        args.add("false");
+        String[] cubeMetaArgs = new String[args.size()];
+        args.toArray(cubeMetaArgs);
+
+        CubeMetaExtractor cubeMetaExtractor = new CubeMetaExtractor();
+        cubeMetaExtractor.execute(cubeMetaArgs);
+
+        File[] files = tempDir.listFiles();
+        Preconditions.checkState(files.length == 1);
+        String testDir = "test.zip";
+        CliCommandExecutor cli = new CliCommandExecutor();
+        String cmd = "cd " + files[0].getParent() + " && zip -r " + testDir + " " + files[0].getName();
+        cli.execute(cmd);
+
+        return files[0].getParent() + "/" +testDir;
     }
 
 }
