0756a55627670a967f6b380d87bcb7b5dc679982
Chao Long <wayne.l@qq.com>
2018-06-12 11:18:02 +0800

    #5444 if there is no 'cluster by' or 'distribute by' column, dont create redistribute flat hive table step

--- source-hive/src/main/java/org/apache/kylin/source/hive/HiveMRInput.java
+++ source-hive/src/main/java/org/apache/kylin/source/hive/HiveMRInput.java
@@ -150,7 +150,9 @@ public class HiveMRInput implements IMRInput {
 
             // then count and redistribute
             if (cubeConfig.isHiveRedistributeEnabled()) {
-                jobFlow.addTask(createRedistributeFlatHiveTableStep(hiveInitStatements, cubeName));
+                if (flatDesc.getClusterBy() != null || flatDesc.getDistributedBy() != null) {
+                    jobFlow.addTask(createRedistributeFlatHiveTableStep(hiveInitStatements, cubeName));
+                }
             }
 
             // special for hive
